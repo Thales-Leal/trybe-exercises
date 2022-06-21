4732e28b831ef66c54fae730f66e53f2d50877de
Thales Leal <thales@wguimaraes.com>
2022-06-21 16:18:38 -0300

    first pratices

---
+++ fundamentos/bloco4-introducao-javascript-logica-programacao/dia1-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,33 @@
+//Variáveis//
+
+const myName = "Thales Leal";
+const birthCity = "Salvador";
+const birthYear = 2002;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+//Tipos Primitivos, Tipagem Dinmâmica e Operações Aritiméticas//
+
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: "Ana",
+  lastName: "Santos",
+};
+const patientEmail = "ana@email.com";
+
+console.log(typeof patientId);
+console.log(typeof patientEmail);
+console.log(typeof patientInfo);
+console.log(typeof isEnrolled);
+console.log(typeof patientAge);
+
+const base = 5;
+let height = 8;
+const area = base * height;
+const perimeter = base + height;
+
+console.log(perimeter * 2);
+console.log(area);
